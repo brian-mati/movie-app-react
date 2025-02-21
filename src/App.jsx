7cdfa921c7c30d65555a01097086d3c84ba9ab61
brian-mati <brianmuthomimati@gmail.com>
2025-02-21 17:27:44 +0300

    style setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 import React from 'react'
-import { useState } from 'react'
-
-const Card = ({title})  =>{
-  const [count,setCount] = useState(0)
-  
-  const [hasLiked,setHasLiked] = useState(false)
-  return(
-      <div>
-        <h1 >{title}
-        </h1>
-        <br />
-        <button onClick={() => setHasLiked(!hasLiked)}>
-          {hasLiked ? "liked":"like"}
-        </button>
-        <br />
-        <h2 onClick={ () => setCount((prevState) => prevState + 1)}>
-          {count}
-        </h2>
-        <h2 onClick={() => setCount((prevState) => prevState - 1 )}>{count}</h2>
-      </div>
-    
-  )
-}
 
 const App = () => {
   return (
-    <div>
-      <Card title= " my king" />
+   <main>
+    <div className='pattern'> 
+      <div className='wrapper'>
+          <header>
+            <h1>Find <span className="text-gradient">movies</span> you'll enjoy without the hassle </h1>
+          </header>
+          <p>Search</p>
+      </div>
     </div>
+   </main>
   )
 }
 
